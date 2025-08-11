d75cd66b87351f12619044a9a0507578d0c3f335
your_github_username <pokhrelameet12@gmail.com>
2025-08-11 11:09:41 +0545

    fixed the images in comnntact pages.

--- backend/bookpandit_django/core/urls.py
+++ backend/bookpandit_django/core/urls.py
@@ -2,7 +2,7 @@ from . import views_payment
 from . import views
 
 urlpatterns = [
-    path('api/verify/esewa/', views_payment.verify_esewa, name='verify_esewa'),
+    pat('api/verify/esewa/', views_payment.verify_esewa, name='verify_esewa'),
     path('api/verify/imepay/', views_payment.verify_imepay, name='verify_imepay'),
     path('api/verify/khalti/', views_payment.verify_khalti, name='verify_khalti'),
     path('api/mark-paid/', views.mark_paid, name='mark_paid'),
