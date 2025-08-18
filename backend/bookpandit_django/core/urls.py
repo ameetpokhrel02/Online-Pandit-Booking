73e6baf7018e581bd04b59edc3aa59bfb4d0e3d4
pokhrelameet12@gmail.com <pokhrelameet12@gmail.com>
2025-08-18 20:00:58 +0545

    modified backend data and urls

--- backend/bookpandit_django/core/urls.py
+++ backend/bookpandit_django/core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.urls import path
 
 urlpatterns = [
-    pat('api/verify/esewa/', views_payment.verify_esewa, name='verify_esewa'),
+    path('api/verify/esewa/', views_payment.verify_esewa, name='verify_esewa'),
     path('api/verify/imepay/', views_payment.verify_imepay, name='verify_imepay'),
     path('api/verify/khalti/', views_payment.verify_khalti, name='verify_khalti'),
     path('api/mark-paid/', views.mark_paid, name='mark_paid'),
