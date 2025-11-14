fc907eef038d18eecb6bebdeb3672d1d28c5d2f3
pokhrelameet12@gmail.com <pokhrelameet12@gmail.com>
2025-11-14 22:21:53 +0545

    add docker_composeyml file

--- backend/pandityatra/pandityatra/wsgi.py
+++ backend/pandityatra/pandityatra/wsgi.py
@@ -19,3 +19,8 @@ os.environ.setdefault('PYTHONPATH', '/app/backend/pandityatra')
 
 application = get_wsgi_application()
 application.debug = True
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
